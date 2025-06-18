a5691b65555494421f9d56b71943086fd16baff1
hourherng <lyhourreseller@gmail.com>
2025-06-18 11:56:16 +0700

    Add files via upload
    Signed-off-by: hourherng <lyhourreseller@gmail.com>

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ function addToCart(itemName) {
 }
 // Telegram
 window.open('https://t.me/openai_support')
+function updateTime() {
+  const now = new Date();
+  const timeStr = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+  document.getElementById('current-time').textContent = timeStr;
+}
 
-
+setInterval(updateTime, 1000); // Update every second
+updateTime(); // Initial call
